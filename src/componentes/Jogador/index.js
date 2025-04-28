88cfb12126ab9f71b778ccaa316639b02822a184
enzirneo <enzo.bispo22@gmail.com>
2025-04-28 15:38:52 -0300

    Primeiro commit

---
+++ src/componentes/Jogador/index.js
@@ -0,0 +1,18 @@
+import './Jogador.css'
+
+const Jogador = ({ nome, função, imagem, corDeFundo }) => {
+    return (
+      <div className='jogador'> 
+        <div className='cabecalho' style={{ backgroundColor: corDeFundo }}>
+          <img src={imagem} alt={nome} />
+        </div>
+        <div className='rodape'>
+          <h4>{nome}</h4>
+          <h5>{função}</h5>
+        </div>
+      </div>
+    )
+  }
+  
+
+export default Jogador
